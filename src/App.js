aa079c4cdb635cc257e59248c0a07265eb6f6702
emirhanzemheri <emirhanzemheri9@gmail.com>
2023-08-15 00:30:01 +0300

    Paho-Mqtt connect not fully correct but run up project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Client } from 'paho-mqtt';
 
 function App() {
+  const mqttOptions = {
+    server: 'localhost',  // Sunucu adresini buraya girin
+    port: 1883,           // WebSocket bağlantı noktasını buraya girin (örneğin, 9001)
+           // WebSocket yolunu belirtin (broker yapılandırmasına uygun olarak)
+  };
+  
+  const client = new Client(mqttOptions.server, Number(mqttOptions.port), 'clientId');
+
+  client.onConnectionLost = (responseObject) => {
+    if (responseObject.errorCode !== 0) {
+      console.error('MQTT bağlantısı kaybedildi:', responseObject.errorMessage);
+    }
+  };
+
+  client.connect({
+    onSuccess: () => {
+      console.log('MQTT bağlantısı başarılı!');
+      const topic = 'mytopic';
+      const message = new window.Paho.MQTT.Message('Merhaba, MQTT!');
+      message.destinationName = topic;
+      client.send(message);
+    },
+    onFailure: (errorCode) => {
+      console.error('MQTT bağlantı hatası:', errorCode.errorCode);
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>MQTT bağlantısı yapılıyor...</p>
     </div>
   );
 }

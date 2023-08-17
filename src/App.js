c90d6b957e8a0004271dea25e1a4b71251cf7aeb
emirhanzemheri <emirhanzemheri9@gmail.com>
2023-08-17 23:59:02 +0300

    Has been added subscribe to command python publisher

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Client } from 'paho-mqtt';
 
 function App() {
-  const mqttOptions = {
-    server: 'localhost',  // Sunucu adresini buraya girin
-    port: 1883,           // WebSocket bağlantı noktasını buraya girin (örneğin, 9001)
-           // WebSocket yolunu belirtin (broker yapılandırmasına uygun olarak)
-  };
-  
-  const client = new Client(mqttOptions.server, Number(mqttOptions.port), 'clientId');
+  const [messages, setMessages] = useState([]);
+  const [mqttClient, setMqttClient] = useState(null); // MQTT istemcisini durum olarak tutun
 
-  client.onConnectionLost = (responseObject) => {
-    if (responseObject.errorCode !== 0) {
-      console.error('MQTT bağlantısı kaybedildi:', responseObject.errorMessage);
+  const mqttOptions = {
+    server: 'localhost', // Broker'ın adresini buraya girin
+    port: 9001,          // Broker'ın WebSocket portunu buraya girin
+    clientId: 'mqtt-react-app', // İstemci kimliği
+  };
+
+  useEffect(() => {
+    const client = new Client(mqttOptions.server, mqttOptions.port, mqttOptions.clientId);
+
+    client.onConnectionLost = (responseObject) => {
+      if (responseObject.errorCode !== 0) {
+        console.log('Bağlantı kesildi:', responseObject.errorMessage);
+      }
+    };
+
+    client.onMessageArrived = (message) => {
+      console.log('Yeni mesaj:', message.payloadString);
+      setMessages((prevMessages) => [...prevMessages, message.payloadString]);
+    };
+
+    client.connect({
+      onSuccess: () => {
+        console.log('MQTT bağlantısı başarılı.');
+        client.subscribe('dataTopic'); // Dinlenecek konuyu belirleyin
+        setMqttClient(client); // MQTT istemcisini duruma ayarla
+      },
+      onFailure: (e) => {
+        console.log('MQTT bağlantısı başarısız:', e);
+      },
+    });
+
+    return () => {
+      client.disconnect();
+    };
+  }, []);
+
+  const publishMessage = () => {
+    if (mqttClient) { // MQTT istemcisini kontrol et
+      const message = 'SendData';
+      mqttClient.publish('reactCommand', message); // Yayınlanacak konuyu ve mesajı belirleyin
+    } else {
+      console.log('Mqtt bağlantısı henüz yapılmadı.');
     }
   };
 
-  client.connect({
-    onSuccess: () => {
-      console.log('MQTT bağlantısı başarılı!');
-      const topic = 'mytopic';
-      const message = new window.Paho.MQTT.Message('Merhaba, MQTT!');
-      message.destinationName = topic;
-      client.send(message);
-    },
-    onFailure: (errorCode) => {
-      console.error('MQTT bağlantı hatası:', errorCode.errorCode);
-    },
-  });
-
   return (
     <div className="App">
-      <p>MQTT bağlantısı yapılıyor...</p>
+      <h1>MQTT Mesajları</h1>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <button onClick={publishMessage}>Mesaj Yayınla</button>
     </div>
   );
 }
